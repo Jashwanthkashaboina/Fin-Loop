6c068a59107f8bda3af8f211e63725883548faa0
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2025-12-28 11:38:11 +0530

    Add static Data to holdings (data.jsx)

--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -1,7 +1,10 @@
+import { holdings } from "../data/data.jsx";
+
+
 function Holdings() {
     return ( 
         <>
-            <h3 className="title">Holdings (13)</h3>
+            <h3 className="title">Holdings ({holdings.length})</h3>
             <div className="order-table">
                 <table>
                 <tr>
@@ -14,20 +17,37 @@ function Holdings() {
                     <th>Net chg.</th>
                     <th>Day chg.</th>
                 </tr>
+                {holdings.map((stock, index) =>{
+                    const currValue = stock.price * stock.qty;
+                    const isProfit = (currValue - stock.avg * stock.qty) >= 0.0;
+                    const profClass = isProfit ? "profit" : "loss";
+                    const dayClass = stock.isLoss ? "loss" : "profit";  
+                    
+                    return (
+                        <tr key={index}>
+                            <td> { stock.name } </td>
+                            <td> { stock.qty } </td>
+                            <td> { stock.avg.toFixed(2) } </td>
+                            <td> { stock.price.toFixed(2) } </td>
+                            <td> { currValue.toFixed(2) } </td>
+                            <td className='profClass'> 
+                                { (currValue - stock.avg * stock.qty).toFixed(2) } 
+                            </td>
+                            <td className={ profClass }> { stock.net } </td>
+                            <td className={ dayClass }> { stock.day }</td>
+                        </tr>
+                    );
+                })}
                 </table>
             </div>
 
             <div className="row">
                 <div className="col">
-                <h5>
-                    29,875.<span>55</span>{" "}
-                </h5>
+                <h5>29,875.<span>55</span>{" "} </h5>
                 <p>Total investment</p>
                 </div>
                 <div className="col">
-                <h5>
-                    31,428.<span>95</span>{" "}
-                </h5>
+                <h5>31,428.<span>95</span>{" "} </h5>
                 <p>Current value</p>
                 </div>
                 <div className="col">
