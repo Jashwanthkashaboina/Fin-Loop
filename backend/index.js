72fe267b33078fa61c1598ca4b3769fefc02ce47
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2025-12-30 12:46:30 +0530

    Connect to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,23 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
+const PORT = process.env.PORT || 8000;
+const dbUrl = process.env.MONGODB_URL;
 
-app.listen(8000, () =>{
+main()
+    .then(() =>{
+        console.log("Connected to Database");
+    })
+    .catch((err) =>{
+        console.log(err);
+    });
+
+async function main() {
+    await mongoose.connect(dbUrl);
+}
+
+app.listen(PORT, () =>{
     console.log("Sever is listening to the port 8080");
 });
