95e8209a6d198c45260a8d53849dd3af758f2edf
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2026-01-09 23:39:30 +0530

    connect dashboard to frontend & Add protectDashboard Route

--- dashboard/src/components/Home.jsx
+++ dashboard/src/components/Home.jsx
@@ -1,13 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import TopBar from "./TopBar";
-import Dashboard from './Dashboard'
+import Dashboard from "./Dashboard";
 
 function Home() {
-    return ( 
-        <>
-            <TopBar/>
-            <Dashboard/>
-        </>
-     );
+  const [checking, setChecking] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await axios.get("http://localhost:8000/me", {
+          withCredentials: true,
+        });
+
+        if (!res.data.loggedIn) {
+          window.location.href = "http://localhost:5173/login";
+        } else {
+          setChecking(false); // allow dashboard to render
+        }
+      } catch (err) {
+        window.location.href = "http://localhost:5173/login";
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  if (checking) {
+    return <h3 style={{ padding: "20px" }}>Loading dashboard...</h3>;
+  }
+
+  return (
+    <>
+      <TopBar />
+      <Dashboard />
+    </>
+  );
 }
 
 export default Home;
